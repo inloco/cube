55fb49dee886a3d4a16a4e888f820da22e9885f4
Clodes Fernando <clodes.fernando@incognia.com>
2024-03-25 11:36:44 -0300

    Delete examples directory
    The examples directory was being analyzed by the dependabot, but it is not used in the production resources.
    So, in order to remove the unnecessary security alerts and reduce the dependabot analysis surface, we are deleting
    the examples directory.

--- examples/recipes/changing-visibility-of-cubes-or-views/schema/TotalRevenuePerCustomer.js
+++
@@ -1,9 +0,0 @@
-view('TotalRevenuePerCustomer', {
-	description: `Total revenue per customer`,
-  shown: COMPILE_CONTEXT.permissions['finance'],
-
-	includes: [
-		Orders.totalRevenue,
-		Users.company,
-	],
-});
--- examples/recipes/lambda-view/cube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  orchestratorOptions: {
-    preAggregationsOptions: {
-      externalRefresh: false,
-    },
-  },
-};
